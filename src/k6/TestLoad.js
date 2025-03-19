42994a246c82d02e35a31f1b7af01e11aea961bd
FERREIRA Luciano <ferreira.luciano@outlook.fr>
2025-03-19 16:07:33 +0100

    added k6 test

---
+++ src/k6/TestLoad.js
@@ -0,0 +1,14 @@
+import http from 'k6/http';
+
+import { sleep } from 'k6';
+
+export const options = {
+    vus: 100,
+    duration: '1m'
+}
+
+export default function () {
+    http.get('http://localhost:8080');
+
+    sleep(1);
+}
